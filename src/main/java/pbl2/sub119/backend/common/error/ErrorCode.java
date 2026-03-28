c383a7764ee07414fe093945fd72b77e7184a44b
Kimhajin <athwls@naver.com>
2026-03-29 01:45:14 +0900

    fix: 에러코드 중복 수정

--- src/main/java/pbl2/sub119/backend/common/error/ErrorCode.java
+++ src/main/java/pbl2/sub119/backend/common/error/ErrorCode.java
@@ -22,7 +22,7 @@ public enum ErrorCode {
     AUTH_USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH004", "존재하지 않는 사용자입니다."),
     AUTH_FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH005", "접근 권한이 없습니다."),
     OAUTH_TOKEN_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "AUTH4006", "소셜 로그인 중 액세스 토큰 요청에 실패했습니다."),
-    IS_NOT_VALID_SOCIAL(HttpStatus.BAD_REQUEST, "AUTH001", "지원하지 않는 플랫폼 입니다"),
+    IS_NOT_VALID_SOCIAL(HttpStatus.BAD_REQUEST, "AUTH4008", "지원하지 않는 플랫폼 입니다"),
     OAUTH_USERINFO_RESPONSE_EMPTY(HttpStatus.BAD_REQUEST, "AUTH4007", "소셜 로그인 중 사용자 정보 응답이 비어 있습니다."),
 
     // SubProduct
